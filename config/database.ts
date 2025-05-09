aa1538674bf4e1e795562d5d963ba2376180d78a
tomi <tomimateo2008@hormail.com>
2025-05-09 19:58:04 -0300

    Primer commit

--- config/database.ts
+++ config/database.ts
@@ -24,20 +24,12 @@ export default ({ env }) => {
     },
     postgres: {
       connection: {
-        connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
+        host: env('DATABASE_HOST', 'db.fkysvvxrkgfntemoglwp.supabase.co'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'ecommerce-store'),
-        user: env('DATABASE_USERNAME', ''),
-        password: env('DATABASE_PASSWORD', '42323986'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-        },
+        database: env('DATABASE_NAME', 'postgres'),
+        user: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', '42323986Tome#'), // Cambia esto por tu contraseña
+        ssl: env.bool('DATABASE_SSL', true), // Habilitamos SSL
         schema: env('DATABASE_SCHEMA', 'public'),
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
